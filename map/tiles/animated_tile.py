9aa20af39c6fdb444b130fb9855ffb2f77014b5a
Karol <karolsiluch@wp.pl>
2024-12-01 20:33:56 +0100

    animated tile

---
+++ map/tiles/animated_tile.py
@@ -0,0 +1,17 @@
+from map.tiles.tile import Tile
+from animation.animation import Animation
+
+
+class AnimatedTile(Tile):
+    def __init__(self, groups, type: str, animations, first_state, render_y_offset: int = 0,
+                 offgrid_tile: bool = False, **pos) -> None:
+
+        self._animations = animations
+        self._current_animation: Animation = self._animations[first_state].copy()
+        super().__init__(groups, type, self._current_animation.img(), render_y_offset, offgrid_tile, **pos)
+
+    def animate(self):
+        self.sprite.new_image(self._current_animation.img())
+
+    def change_animation(self, state):
+        self._current_animation = self._animations[state].copy()
